bfc40b69005d72b65c505471907649b08250c9b1
Diego <diegob96@uchicago.edu>
2022-12-12 23:10:00 -0800

    long long ints were the key

--- day11/day11.c
+++ day11/day11.c
@@ -3,21 +3,30 @@
 #include <string.h>
 #include <ctype.h>
 #define LINE_LENGTH 256
-#define DIV_ROUND_CLOSEST(n, d) ((((n) < 0) ^ ((d) < 0)) ? (((n) - (d)/2)/(d)) : (((n) + (d)/2)/(d)))
 
-int power(int base, int exp) {
+long long int power(long long int base, long long int exp) {
     if (exp == 0)
         return 1;
     else if (exp % 2)
         return base * power(base, exp - 1);
     else {
-        int temp = power(base, exp / 2);
+        long long int temp = power(base, exp / 2);
         return temp * temp;
     }
 }
 
+int lcm(int a, int b) {
+    int gcd;
+    for (int i = 1; i <= a && i <= b; ++i) {
+        // check if i is a factor of both integers
+        if (a % i == 0 && b % i == 0)
+            gcd = i;
+    }
+    return ((a * b)/ gcd);
+}
+
 typedef struct Object {
-    int worry;
+    long long int worry;
     struct Object* next;
 } Object;
 
@@ -119,17 +128,21 @@ Monkey* read_monkey(char* first, FILE* file, Monkey** monkeys) {
 }
 
 void add_object(Object* object, Monkey** mon) {
-    Object* c_obj = (*mon) -> objects;
     int num_objects = (*mon) -> num_objects;
-    for (int i = 0; i< num_objects; i++) {
-        c_obj = c_obj -> next;
+    if (num_objects == 0) {
+        (*mon) -> objects = object;
+    } else {
+        Object* c_obj = (*mon) -> objects;
+        for (int i = 1; i< num_objects; i++) {
+            c_obj = c_obj -> next;
+        }
+        c_obj -> next = object;
     }
-    c_obj -> next = object;
     (*mon) -> num_objects ++;
     return;
 }
 
-int inspect_item(Monkey** mon) {
+int inspect_item_1(Monkey** mon) {
     if ((*mon) -> num_objects == 0) {
         return -1;
     }
@@ -138,8 +151,7 @@ int inspect_item(Monkey** mon) {
     (*mon) -> num_objects --;
     (*mon) -> inspected ++;
     (*mon) -> objects = n_obj;
-    int worry = c_obj -> worry;
-    printf("%i\n",worry);
+    long long int worry = c_obj -> worry;
     char type = (*mon) -> op_type;
     if (type == '+') {
         worry += ((*mon) -> op_num);
@@ -148,12 +160,43 @@ int inspect_item(Monkey** mon) {
     } else if (type == '^') {
         worry = power(worry,((*mon) -> op_num));
     }
-    worry = DIV_ROUND_CLOSEST(worry, 3);
+    worry = worry/3;
     c_obj -> worry = worry;
+    //printf("%i\n",worry);
     if (worry % ((*mon)->div_test) == 0) {
         add_object(c_obj, (*mon)->if_true);
     } else {
+        add_object(c_obj, (*mon)->if_false);
+    }  
+    return 0;
+}
+
+int inspect_item_2(Monkey** mon, int mcm) {
+    if ((*mon) -> num_objects == 0) {
+        return -1;
+    }
+    Object* c_obj = (*mon) -> objects;
+    Object* n_obj = c_obj -> next;
+    (*mon) -> num_objects --;
+    (*mon) -> inspected ++;
+    (*mon) -> objects = n_obj;
+    long long int worry = c_obj -> worry;
+    char type = (*mon) -> op_type;
+    if (type == '+') {
+        worry += ((*mon) -> op_num);
+    } else if (type == '*') {
+        worry *= ((*mon) -> op_num);
+    } else if (type == '^') {
+        worry = power(worry,((*mon) -> op_num));
+    }
+    //printf("A: %lli\n",worry);
+    worry = worry % mcm;
+    c_obj -> worry = worry;
+    //printf("B: %lli\n",worry);
+    if (worry % ((*mon)->div_test) == 0) {
         add_object(c_obj, (*mon)->if_true);
+    } else {
+        add_object(c_obj, (*mon)->if_false);
     }  
     return 0;
 }
@@ -161,7 +204,7 @@ int inspect_item(Monkey** mon) {
 int main()
 {
     FILE *file;
-    int test = 1;
+    int test = 0;
     if (test == 1) {
         file= fopen("test.txt", "r");
     } else {
@@ -177,14 +220,13 @@ int main()
     Monkey* c_monkey;
 
     while (fgets(line, LINE_LENGTH, file)) {
-        printf("%s", line);
         monkeys[num_monkeys] = read_monkey(line, file, monkeys);
         num_monkeys ++;
         fgets(line, LINE_LENGTH, file);
     }
-    printf("Read %i monkeys.\n", num_monkeys);
+    printf("Read %i monkeys in part 1.\n", num_monkeys);
 
-    int rounds = 1;
+    int rounds = 20;
     int items;
     int i;
     int j;
@@ -194,23 +236,58 @@ int main()
             c_monkey = monkeys[j];
             items = c_monkey->num_objects;
             for(k=0;k<items;k++) {
-                inspect_item(&c_monkey);
+                inspect_item_1(&c_monkey);
             }
         }
     }
+    int winner[2] = {0,0};
+    int inspection;
+    for(j=0; j<num_monkeys; j++) {
+        c_monkey = monkeys[j];
+        inspection = c_monkey->inspected;
+        if(inspection > winner[0]) {
+            winner[1] = winner[0];
+            winner[0] = inspection;
+        } else if (inspection > winner[1]) {
+            winner[1] = inspection;
+        }
+    }
+    printf("The total amount of monkey bussiness in part 1 is %i.\n", (winner[0]*winner[1]));
 
-    c_monkey = monkeys[1];
-    printf("%i.\n", (c_monkey->objects)->worry);
+
+    rewind(file);
+    num_monkeys = 0;
+    int mcm = 1;
+    Monkey** monkeys_2 = (Monkey **) calloc(64, sizeof(Monkey*));
+    while (fgets(line, LINE_LENGTH, file)) {
+        monkeys_2[num_monkeys] = read_monkey(line, file, monkeys_2); 
+        c_monkey = monkeys_2[num_monkeys];
+        mcm = lcm(mcm, c_monkey->div_test);
+        fgets(line, LINE_LENGTH, file);
+        num_monkeys ++;
+    }
+    printf("Read %i monkeys in part 2. LCM is %i\n", num_monkeys, mcm);
+    rounds = 10000;
+    for (i=0; i<rounds; i++) {
+        for(j=0; j<num_monkeys; j++) {
+            c_monkey = monkeys_2[j];
+            items = c_monkey->num_objects;
+            for(k=0;k<items;k++) {
+                inspect_item_2(&c_monkey, mcm);
+            }
+        }
+    }
+    long long int winner_2[2] = {0,0};
+    for(j=0; j<num_monkeys; j++) {
+        c_monkey = monkeys_2[j];
+        inspection = c_monkey->inspected;
+        if(inspection > winner_2[0]) {
+            winner_2[1] = winner_2[0];
+            winner_2[0] = inspection;
+        } else if (inspection > winner_2[1]) {
+            winner_2[1] = inspection;
+        }
+    }
+    printf("The total amount of monkey bussiness in part 2 is %lli times %lli, so %lli.\n", winner_2[0], winner_2[1], (winner_2[0]*winner_2[1]));
     return 0;
 }
-
-
-
-
-
-
-            
-
-
-
-
